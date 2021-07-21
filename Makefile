39090771250b5671edc37beb80fc350537aa881c
Yi Lee <ylee1228@umd.edu>
2021-07-21 10:10:13 +0000

    Implement Montgomery modular multiplication

---
+++ Makefile
@@ -0,0 +1,10 @@
+main: main.cpp montgomery.s
+
+%.japp: %.jazz
+	gpp -o $@ $<
+
+%.s: %.japp
+	jasminc -pasm $< > $@
+
+clean:
+	rm -f *.japp *.s
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#define PRINT(X) cout << (#X) << " = " << (X) << endl
+
+extern "C" {
+	uint64_t montgomery(uint32_t x);
+	uint32_t montgomery_REDC(uint64_t mx);
+	uint64_t brute_force_mod64(uint64_t lhs, uint64_t rhs);
+}
+
+const uint64_t dilithium_N = (1 << 23) - (1 << 13) + 1;
+
+int main() {
+	uint64_t z = 823719;
+	PRINT(z);
+	uint64_t mZ = montgomery(z);
+	PRINT(montgomery_REDC(mZ));
+
+	uint32_t x = 374824;
+	uint32_t y = 8047392;
+	uint64_t mX = montgomery(x);
+	uint64_t mY = montgomery(y);
+	uint64_t mProd = montgomery_REDC(mX * mY);
+	uint64_t prod = montgomery_REDC(mProd);
+	PRINT(prod);
+
+	uint64_t answer = (uint64_t(x) * y) % dilithium_N;
+	PRINT(answer);
+
+	return 0;
+}
