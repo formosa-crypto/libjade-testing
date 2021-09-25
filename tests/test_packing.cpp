75a14b55a13af52bedf31ee36a6a6b58046c7560
Yi Lee <ylee1228@umd.edu>
2021-09-25 08:07:17 +0000

    Figure out how to call functions from ref impl

--- tests/test_packing.cpp
+++ tests/test_packing.cpp
@@ -5,6 +5,7 @@
 extern "C" {
 #include "../dilithium/ref/api.h"
 #include "../dilithium/ref/params.h"
+#include "../dilithium/ref/poly.h"
 }
 
 using std::cout;
@@ -14,26 +15,45 @@ using std::memcmp;
 
 #define PRINT(X) cout << (#X) << " = " << (X) << endl
 
-extern "C" {
-	void gen_noises_entry_test();
-	void gen_noises_test(uint32_t s1[L * N]);
-}
-
-/*
-uint8_t sampleByte() {
+uint32_t sample_t1_component() {
 	static std::random_device rd;
 	static std::mt19937 gen(rd());
-	static std::uniform_int_distribution<> distrib(0,  255);
+	static std::uniform_int_distribution<> distrib(0,  1023);
 	return distrib(gen);
 }
-*/
 
 int main() {
-	uint32_t s1[L * N] = { 0 };
-	gen_noises_test(s1);
-	for(int i = 0; i < 20; ++i) {
-		PRINT(s1[i]);
+	uint8_t pk_ref[pqcrystals_dilithium3_PUBLICKEYBYTES];
+	uint8_t sk_ref[pqcrystals_dilithium3_SECRETKEYBYTES];
+
+	uint8_t randomness[32] = { 0 };
+	/*
+	for(int i = 0; i < 32; ++i) {
+		randomness[i] = sampleByte();
+	}
+	*/
+
+	pqcrystals_dilithium3_ref_seeded_keypair(pk_ref, sk_ref, randomness);
+
+
+	/*
+	PRINT(memcmp(sk_ref, sk_jazz, 64));
+	PRINT(memcmp(sk_ref + 96, sk_jazz + 96, L * N / 2 + K * N / 2));
+	PRINT(memcmp(pk_ref, pk_jazz, 32));
+	PRINT(memcmp(pk_ref, pk_jazz, pqcrystals_dilithium3_PUBLICKEYBYTES));
+	*/
+
+	poly p;
+	for(int i = 0; i < N; ++i) {
+		p.coeffs[i] = sample_t1_component();
 	}
 
+	uint8_t buf[POLYT1_PACKEDBYTES];
+
+	pqcrystals_dilithium3_ref_polyt1_pack(buf, &p);
+
+	PRINT(int(buf[0]));
+	PRINT(int(buf[1]));
+	
 	return 0;
 }
