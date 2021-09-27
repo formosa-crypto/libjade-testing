a63e44b76fb36e110e7b675b08a31b6cc361c827
Yi Lee <ylee1228@umd.edu>
2021-09-27 03:21:25 +0000

    Rewrite FFT according to ref impl

--- tests/test_fft.cpp
+++ tests/test_fft.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <cstring>
+#include <array>
 #include <string>
 
 extern "C" {
@@ -15,50 +16,20 @@ using std::vector;
 using std::memcmp;
 using std::runtime_error;
 using std::to_string;
+using std::array;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
 
 #define PRINT(X) cout << (#X) << " = " << (X) << endl
 
 extern "C" {
 	void fft_jazz(uint32_t f[N]);
-	void ifft_jazz(uint32_t f[N]);
-	void ifft_to_mont_jazz(uint32_t f[N]);
-	uint32_t montgomery_REDC_jazz(uint64_t mx);
-	void fft_poly_mult_jazz(uint32_t f[N], uint32_t g[N], uint32_t fg[N]);
+	//void ifft_to_mont_jazz(uint32_t f[N]);
+	//uint32_t montgomery_REDC_jazz(uint64_t mx);
+	//void fft_poly_mult_jazz(uint32_t f[N], uint32_t g[N], uint32_t fg[N]);
 }
 
-uint32_t montgomery(uint32_t x) {
-	return (uint64_t(x) << 32) % Q;
-}
-
-// Precompute roots of unity
-// Results are in Montgomery form
-vector<uint64_t> precomputeMRoots() {
-	vector<uint64_t> m_roots;
-	uint32_t prim_root = 1753;
-	uint64_t m_prim_root = montgomery(prim_root);
-	
-	uint64_t mr = montgomery(1);
-	
-	for(int i = 0; i < 512; ++i) {
-		m_roots.push_back(mr);
-		mr = montgomery_REDC_jazz(mr * m_prim_root);
-	}
-	return m_roots;
-}
-
-int bitreverse(int x, int numbits) {
-	int result = 0;
-	for(int i = 0; i < numbits; ++i) {
-		int bit = (x >> i) & 1;
-		result |= bit << (numbits - 1 - i);
-	}
-	return result;
-}
-
-template<typename T> void zero_poly(T f[N]) {
-	for(int i = 0; i < N; ++i)
-		f[i] = 0;
-}
 
 template<typename T> void print_poly(T f[N]) {
 	for(int i = 0; i < 16; ++i) {
@@ -70,235 +41,48 @@ template<typename T> void print_poly(T f[N]) {
 	}
 }
 
-/* Not Montgomery */
-void multiply_polys(uint32_t f[N], uint32_t g[N], uint32_t product[N]) {
-	zero_poly(product);
-	for(int i = 0; i < N; ++i) {
-		for(int fdeg = 0; fdeg < N; ++fdeg) {
-			vector<int> gdegs { i - fdeg, i - fdeg + N };
-			for(int gdeg : gdegs) {
-				if(gdeg >= 0 && gdeg < N) {
-					uint64_t prod = uint64_t(f[fdeg]) * g[gdeg];
-					prod %= Q;
-					product[i] += prod;
-					product[i] %= Q;
-				}
-			}
-		}
-	}
-}
-
-void test_montgomery() {
-	uint64_t z = 823719;
-	uint64_t mZ = montgomery(z);
-
-	if(z != montgomery_REDC_jazz(mZ))
-		throw runtime_error("test failed at " + to_string(__LINE__));
-
-	uint32_t x = 374824;
-	uint32_t y = 8047392;
-	uint64_t mX = montgomery(x);
-	uint64_t mY = montgomery(y);
-	uint64_t mProd = montgomery_REDC_jazz(mX * mY);
-	uint64_t prod = montgomery_REDC_jazz(mProd);
-	uint64_t answer = (uint64_t(x) * y) % Q;
-
-	if(prod != answer)
-		throw runtime_error("test failed at " + to_string(__LINE__));
-}
-
-void test_poly_product() {
-	uint32_t f[N] = { 15 };
-	uint32_t g[N];
-	zero_poly(f);
-
-	if(vector<uint32_t>(f, f + N) != vector<uint32_t>(N, 0))
-		throw runtime_error("test failed at " + to_string(__LINE__));
-
-	zero_poly(g);
-	f[0] = 1;
-	f[1] = 1;
-	g[0] = 1;
-	g[1] = 1;
-	
-	uint32_t prod[N];
-	multiply_polys(f, g, prod);
-
-	if(vector<uint32_t>(prod, prod + 5) != vector<uint32_t> {1, 2, 1, 0, 0})
-		throw runtime_error("test failed at " + to_string(__LINE__));
-
-	g[0] = 2;
-	multiply_polys(f, g, prod);
-	if(vector<uint32_t>(prod, prod + 5) != vector<uint32_t> {2, 3, 1, 0, 0})
-		throw runtime_error("test failed at " + to_string(__LINE__));
-
-	f[2] = 3;
-	multiply_polys(f, g, prod);
-	if(vector<uint32_t>(prod, prod + 5) != vector<uint32_t> {2, 3, 7, 3, 0})
-		throw runtime_error("test failed at " + to_string(__LINE__));
-}
-
-void test_ifft() {
-	auto mroots = precomputeMRoots();
-	uint32_t f[N], mf[N];
-
-	zero_poly(f);
-	f[0] = 1;
-	f[1] = 31;
-	f[2] = 5;
-	f[3] = 200;
-
+array<uint32_t, N> random_poly() {
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> distrib(0, Q - 1);
+	array<uint32_t, N> arr;
 	for(int i = 0; i < N; ++i)
-		mf[i] = montgomery(f[i]);
-
-	fft_jazz(mf);
-	ifft_jazz(mf);
-
-	vector<uint32_t> answer {
-		montgomery(1),
-		montgomery(31),
-		montgomery(5),
-		montgomery(200),
-		0,
-		0,
-		0
-	};
-
-	if(vector<uint32_t>(mf, mf + 7) != answer)
-		throw runtime_error("test failed at " + to_string(__LINE__));
-
-	uint32_t g[N], mg[N];
-
-	zero_poly(g);
-	g[0] = 82;
-	g[1] = 50;
-	g[2] = 5;
-	g[3] = 591;
-	for(int i = 0; i < N; ++i)
-		mg[i] = montgomery(g[i]);
-
-	fft_jazz(mf);
-	fft_jazz(mg);
-
-	uint32_t mfg[N];
-	fft_poly_mult_jazz(mf, mg, mfg);
-
-	/*
-	PRINT(mf[0]);
-	PRINT(mg[0]);
-	PRINT(mf[1]);
-	PRINT(mg[1]);
-	PRINT(montgomery_REDC_jazz(uint64_t(mf[0]) * mg[0]));
-	PRINT(mfg[0]);
-	PRINT(montgomery_REDC_jazz(uint64_t(mf[1]) * mg[1]));
-	PRINT(mfg[1]);
-	*/
-
-	ifft_jazz(mfg);
-
-	/*
-	cout << "mfg = " << endl;
-	print_poly(mfg);
-	*/
-
-	uint32_t fg[N];
-	multiply_polys(f, g, fg);
-
-	/*
-	cout << "fg = " << endl;
-	print_poly(fg);
-	*/
-
-	for(int i = 0; i < N; ++i)
-		if(montgomery(fg[i]) != mfg[i])
-			throw runtime_error("test failed at " + to_string(__LINE__));
-
+		arr[i] = distrib(gen);
+	return arr;
 }
 
 void test_fft() {
+	auto arr = random_poly();
 	uint32_t f[N];
-	zero_poly(f);
-	f[0] = montgomery(1);
+	int32_t g[N];
+	for(int i = 0; i < N; ++i) {
+		f[i] = arr[i];
+		g[i] = arr[i];
+	}
 	fft_jazz(f);
+	ntt(g);
 
-	if(vector<uint32_t>(f, f + N) != vector<uint32_t>(N, montgomery(1)))
-		throw runtime_error("test failed at " + to_string(__LINE__));
-
-	uint32_t g[N];
-	zero_poly(g);
-	g[1] = montgomery(1);
-	fft_jazz(g);
-
-	auto mRoots = precomputeMRoots();
-	for(int i = 0; i < 10; ++i) {
-		if(mRoots[bitreverse(i, 9)] != g[i])
-			throw runtime_error("test failed at " + to_string(__LINE__));
+	uint32_t uint_g[N];
+	for(int i = 0; i < N; ++i) {
+		uint_g[i] = (g[i] % Q + Q) % Q;
 	}
 
-	if(mRoots[bitreverse(N - 1, 9)] != g[N - 1])
+	cout << "f =" << endl;
+	print_poly(f);
+
+	cout << endl << "g =" << endl;
+	print_poly(uint_g);
+
+	if(memcmp(f, uint_g, 4 * N) != 0)
 		throw runtime_error("test failed at " + to_string(__LINE__));
-
-	if(mRoots[bitreverse(N - 2, 9)] != g[N - 2])
-		throw runtime_error("test failed at " + to_string(__LINE__));
-	
-	uint32_t h[N];
-
-	zero_poly(h);
-	h[0] = montgomery(1);
-	h[1] = montgomery(1);
-
-	fft_jazz(h);
-
-	for(int i = 0; i < 10; ++i) {
-		if((f[i] + g[i]) % Q != h[i])
-			throw runtime_error("test failed at " + to_string(__LINE__));
-	}
 }
 
 void test_ifft_to_mont() {
-	uint32_t f[N] = { 1, 31, 5, 200 };
-
-
-	uint32_t g[N] = { 82, 50, 5, 591 };
-
-	uint32_t fg[N] = { 0 };
-
-	multiply_polys(f, g, fg);
-
-	//cout << endl << "fg:" << endl;
-	//print_poly(fg);
-
-	fft_jazz(f);
-	fft_jazz(g);
-
-	uint32_t fg_jazz[N];
-	
-	fft_poly_mult_jazz(f, g, fg_jazz);
-
-	ifft_to_mont_jazz(fg_jazz);
-
-	/*
-	cout << "fg_jazz:" << endl;
-	print_poly(fg_jazz);
-	*/
-
-	for(int i = 0; i < N; ++i)
-		if(fg[i] != fg_jazz[i])
-			throw runtime_error("test failed at " + to_string(__LINE__));
-}
-
-void test_problematic_poly() {
-//void ntt(int32_t a[N]) {
-
 
 }
 
 int main() {
-	test_montgomery();
-	test_poly_product();
 	test_fft();
-	test_ifft();
 	test_ifft_to_mont();
-	test_problematic_poly();
 	return 0;
 }
