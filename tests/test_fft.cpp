0d5d83c1fda82c3856b73ea48d5552c168178e9c
Yi Lee <ylee1228@umd.edu>
2021-09-27 03:39:40 +0000

    Reimplement IFFT based on ref impl

--- tests/test_fft.cpp
+++ tests/test_fft.cpp
@@ -25,7 +25,7 @@ using std::uniform_int_distribution;
 
 extern "C" {
 	void fft_jazz(uint32_t f[N]);
-	//void ifft_to_mont_jazz(uint32_t f[N]);
+	void ifft_to_mont_jazz(uint32_t f[N]);
 	//uint32_t montgomery_REDC_jazz(uint64_t mx);
 	//void fft_poly_mult_jazz(uint32_t f[N], uint32_t g[N], uint32_t fg[N]);
 }
@@ -67,18 +67,43 @@ void test_fft() {
 		uint_g[i] = (g[i] % Q + Q) % Q;
 	}
 
-	cout << "f =" << endl;
-	print_poly(f);
 
-	cout << endl << "g =" << endl;
-	print_poly(uint_g);
-
-	if(memcmp(f, uint_g, 4 * N) != 0)
+	if(memcmp(f, uint_g, 4 * N) != 0) {
+		cout << "f =" << endl;
+		print_poly(arr.data());
+		cout << endl << "fft_f =" << endl;
+		print_poly(f);
+		cout << endl << "fft_g =" << endl;
+		print_poly(uint_g);
 		throw runtime_error("test failed at " + to_string(__LINE__));
+	}
 }
 
 void test_ifft_to_mont() {
+	auto arr = random_poly();
+	uint32_t f[N];
+	int32_t g[N];
+	for(int i = 0; i < N; ++i) {
+		f[i] = arr[i];
+		g[i] = arr[i];
+	}
+	ifft_to_mont_jazz(f);
+	invntt_tomont(g);
 
+	uint32_t uint_g[N];
+	for(int i = 0; i < N; ++i) {
+		uint_g[i] = (g[i] % Q + Q) % Q;
+	}
+
+	if(memcmp(f, uint_g, 4 * N) != 0) {
+		cout << "f =" << endl;
+		print_poly(arr.data());
+		cout << endl << "ifft_f =" << endl;
+		print_poly(f);
+		cout << endl << "ifft_g =" << endl;
+		print_poly(uint_g);
+		throw runtime_error("test failed at " + to_string(__LINE__));
+	}
 }
 
 int main() {
