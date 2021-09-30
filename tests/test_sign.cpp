78b30cc6c080374728a986d2e9edfc7d8ffb0428
Yi Lee <ylee1228@umd.edu>
2021-09-30 23:31:25 +0000

    Make test for sign

---
+++ tests/test_sign.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <random>
+#include <cstring>
+
+extern "C" {
+#include "../dilithium/ref/api.h"
+#include "../dilithium/ref/params.h"
+}
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::memcmp;
+
+#define PRINT(X) cout << (#X) << " = " << (X) << endl
+
+extern "C" {
+	void sign_jazz(uint8_t signature[pqcrystals_dilithium3_BYTES],
+			uint8_t* msg,
+			uint64_t m_len,
+			uint8_t sk[pqcrystals_dilithium3_SECRETKEYBYTES]);
+}
+
+uint8_t sampleByte() {
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	static std::uniform_int_distribution<> distrib(0,  255);
+	return distrib(gen);
+}
+
+int main() {
+	uint8_t pk[pqcrystals_dilithium3_PUBLICKEYBYTES];
+	uint8_t sk[pqcrystals_dilithium3_SECRETKEYBYTES];
+
+	uint8_t randomness[32] = { 0 };
+	for(int i = 0; i < 32; ++i)
+		randomness[i] = sampleByte();
+
+	pqcrystals_dilithium3_ref_seeded_keypair(pk, sk, randomness);
+
+	uint8_t m[1000];
+	for(int i = 0; i < 1000; ++i)
+		m[i] = sampleByte();
+
+
+	uint8_t signature_ref[pqcrystals_dilithium3_BYTES];
+	uint8_t signature_jazz[pqcrystals_dilithium3_BYTES];
+
+	sign_jazz(signature_jazz, m, 1000, sk);
+
+	size_t siglen;
+
+	pqcrystals_dilithium3_ref_signature(signature_ref, &siglen, m, 1000, sk);
+
+	PRINT(int(signature_ref[0]));
+	PRINT(int(signature_ref[1]));
+	PRINT(int(signature_ref[2]));
+
+	PRINT(memcmp(signature_ref, signature_jazz, pqcrystals_dilithium3_BYTES));
+
+	return 0;
+}
