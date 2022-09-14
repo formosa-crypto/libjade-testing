a098e708ab66a197d4c12290ef684670593e5eeb
Amber Sprenkels <amber@electricdusk.com>
2022-09-14 17:56:50 +0200

    test: Also build other dilithium variants

--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Scratch test file for testing the integration of the other Dilithium variants.
 #
@@ -8,7 +8,7 @@
 export LD_LIBRARY_PATH="$HOME/jasmin-dilithium/dilithium/ref:$LD_LIBRARY_PATH"
 export TESTS="test_keygen test_fft test_packing test_hashing test_sign_fragments test_expandmask test_sign"
 
-make --jobs=8 --directory=/home/dsprenkels/jasmin-dilithium/dilithium/ref libpqcrystals_dilithium3_ref.so libpqcrystals_fips202_ref.so || exit 125
+make --jobs=8 --directory=/home/dsprenkels/jasmin-dilithium/dilithium/ref libpqcrystals_dilithium{2,3,5}_ref.so libpqcrystals_fips202_ref.so || exit 125
 make --jobs=8 --directory=/home/dsprenkels/jasmin-dilithium/src/dilithium3 || exit 1
 make --jobs=8 --trace || exit 1
 for test_name in $TESTS; do
