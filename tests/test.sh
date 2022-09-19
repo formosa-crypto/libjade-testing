aed431e06778a2820a060aab582df0f750f2b7b9
Amber Sprenkels <amber@electricdusk.com>
2022-09-19 16:44:49 +0200

    Move all lib code into a single common.jazz file

--- tests/test.sh
+++ tests/test.sh
@@ -8,9 +8,9 @@
 export LD_LIBRARY_PATH="$HOME/jasmin-dilithium/dilithium/ref:$LD_LIBRARY_PATH"
 export TESTS="test_keygen test_fft test_packing test_hashing test_sign_fragments test_expandmask test_sign"
 
-make --jobs=8 --directory=/home/dsprenkels/jasmin-dilithium/dilithium/ref libpqcrystals_dilithium{2,3,5}_ref.so libpqcrystals_fips202_ref.so || exit 125
-make --jobs=8 --directory=/home/dsprenkels/jasmin-dilithium/src/dilithium3 || exit 1
-make --jobs=8 --trace || exit 1
+make --directory=/home/dsprenkels/jasmin-dilithium/dilithium/ref libpqcrystals_dilithium{2,3,5}_ref.so libpqcrystals_fips202_ref.so || exit 125
+make --directory=/home/dsprenkels/jasmin-dilithium/src/dilithium3 || exit 1
+make --trace || exit 1
 for test_name in $TESTS; do
     (set -x; "./$test_name" || exit 1)
 done
