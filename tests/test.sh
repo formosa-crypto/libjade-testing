581e50b5415ef997efcef7cf4cf9827a7dd0a08c
Amber Sprenkels <amber@electricdusk.com>
2022-09-29 17:12:50 +0200

    Use array for tests list

--- tests/test.sh
+++ tests/test.sh
@@ -6,14 +6,23 @@
 # Date: 14 Sep 2022
 
 export LD_LIBRARY_PATH="$HOME/jasmin-dilithium/dilithium/ref:$LD_LIBRARY_PATH"
-readonly DEFAULT_TESTS="test_keygen test_fft test_packing test_hashing test_sign_fragments test_expandmask test_sign test_rounding"
-export TESTS=${TESTS:-$DEFAULT_TESTS}
+readonly DEFAULT_TESTS=(
+    test_keygen
+    test_fft
+    test_packing
+    test_hashing
+    test_sign_fragments
+    test_expandmask
+    test_sign
+    test_rounding
+)
+export TESTS=("${TESTS[@]:-${DEFAULT_TESTS[@]}}")
 
 make --directory=/home/dsprenkels/jasmin-dilithium/dilithium/ref libpqcrystals_dilithium{2,3,5}_ref.so libpqcrystals_fips202_ref.so || exit 125
-make --trace || exit 1
+make --trace "${TESTS[@]}" || exit 1
 
-for test_name in $TESTS; do
-    "./$test_name" || echo -e "\033[1;31mtest failed: $test_name\033[0m"
+for test_name in "${TESTS[@]}"; do
+    (set -x; "./$test_name" || echo -e "\033[1;31mtest failed: $test_name\033[0m" )
 done
 
 echo >&2 "Done!"
