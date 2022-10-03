e61a8c4950f8b035d00e380b355c665264329e6f
Amber Sprenkels <amber@electricdusk.com>
2022-10-03 12:26:53 +0200

    Remove some 'extra assignment' warnings

--- tests/test.sh
+++ tests/test.sh
@@ -17,13 +17,17 @@ readonly DEFAULT_TESTS=(
     test_rounding
 )
 export TESTS=("${TESTS[@]:-${DEFAULT_TESTS[@]}}")
+LOGFILE=$(mktemp --tmpdir jasmin.XXXXXXXXXX.log)
 
 make --directory=/home/dsprenkels/jasmin-dilithium/dilithium/ref libpqcrystals_dilithium{2,3,5}_ref.so libpqcrystals_fips202_ref.so || exit 125
 make --trace "${TESTS[@]}" || exit 1
 
 for test_name in "${TESTS[@]}"; do
-    (set -x; "./$test_name" || echo -e "\033[1;31mtest failed: $test_name\033[0m" )
+    (set -x; "./$test_name" || echo -e "\033[1;31mtest failed: $test_name\033[0m")
 done
 
-echo >&2 "Done!"
+echo >&2 "Tests done!"
+(set -x; jasminc -wea "../src/${VARIANT:-dilithium2}/lib.jazz" >/dev/null 2>"$LOGFILE")
+echo "$(grep -c -F 'extra assignment introduced' "$LOGFILE") extra assignment warnings in $LOGFILE"
+
 exit 0
